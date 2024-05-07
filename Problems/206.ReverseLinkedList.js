81660796975c1e694f024bc88f335daeb9cce8e1
james <jamesmdang.bui@gmail.com>
2024-05-06 20:57:13 -0700

    05/06/2024

---
+++ Problems/206.ReverseLinkedList.js
@@ -0,0 +1,30 @@
+// Given the head of a singly linked list, reverse the list, and return the reversed list.
+
+ 
+
+// Example 1:
+
+
+// Input: head = [1,2,3,4,5]
+// Output: [5,4,3,2,1]
+// Example 2:
+
+
+// Input: head = [1,2]
+// Output: [2,1]
+// Example 3:
+
+// Input: head = []
+// Output: []
+
+var reverseList = function(head) {
+    let prev = null
+    while(head !== null) {
+        let temp = head.next
+        head.next = prev
+        prev = head
+        head = temp
+    }
+    return prev
+  };
+  

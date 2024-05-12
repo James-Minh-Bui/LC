99eee807f3ab2e644a18a8ef44b6dd429ec4918a
james <jamesmdang.bui@gmail.com>
2024-05-11 21:14:24 -0700

    05/11/2024

---
+++ Problems/437.PathSumIII.js
@@ -0,0 +1,29 @@
+// Given the root of a binary tree and an integer targetSum, return the number of paths where the sum of the values along the path equals targetSum.
+
+// The path does not need to start or end at the root or a leaf, but it must go downwards (i.e., traveling only from parent nodes to child nodes).
+
+ 
+
+// Example 1:
+
+
+// Input: root = [10,5,-3,3,2,null,11,3,-2,null,1], targetSum = 8
+// Output: 3
+// Explanation: The paths that sum to 8 are shown.
+// Example 2:
+
+// Input: root = [5,4,8,11,null,13,4,7,2,null,null,5,1], targetSum = 22
+// Output: 3
+ 
+var pathSum = function (root, targetSum) {
+    return dfs(root, 0, { 0: 1 });
+    function dfs(node, curSum, sumMap) {
+        if (!node) return 0
+        curSum += node.val
+        let count = sumMap[curSum - targetSum] || 0;   
+        sumMap[curSum] = (sumMap[curSum] || 0) + 1 ;   
+        count += dfs(node.left, curSum, sumMap) + dfs(node.right, curSum, sumMap); 
+        sumMap[curSum]-- ;   
+        return count;
+    }
+};

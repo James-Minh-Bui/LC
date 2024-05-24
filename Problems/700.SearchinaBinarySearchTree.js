9f094df728acfe5565a89f89d3b75a4bebc5dcea
james <jamesmdang.bui@gmail.com>
2024-05-23 20:52:25 -0700

    05/21/2024

---
+++ Problems/700.SearchinaBinarySearchTree.js
@@ -0,0 +1,27 @@
+// You are given the root of a binary search tree (BST) and an integer val.
+
+// Find the node in the BST that the node's value equals val and return the subtree rooted with that node. If such a node does not exist, return null.
+
+ 
+
+// Example 1:
+
+
+// Input: root = [4,2,7,1,3], val = 2
+// Output: [2,1,3]
+// Example 2:
+
+
+// Input: root = [4,2,7,1,3], val = 5
+// Output: []
+
+var searchBST = function(root, val) {
+    let queue = [root]
+    while (stack.length > 0) {
+        let curr = stack.pop()
+        if (curr.val === val) return curr
+        if (curr.left !== null) queue.push(curr.left)
+        if (curr.right !== null) queue.push(curr.right)
+    }
+    return null
+};
